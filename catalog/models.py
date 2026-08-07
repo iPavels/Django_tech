63fd02ac09cf7589c74410cd4b400dfdb62a7fb3
ipavels <sirzaitsev@yandex.ru>
2026-08-07 16:05:56 +0300

    model_Cat

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Product(models.Model):
-    name=models.CharField(max_length=100, verbose_name='наименование', help_text='Введите наименование')
-    description=models.TextField(max_length=500, verbose_name='описание')
-    image=models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='изображение')
+    name=models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование')
+    description=models.TextField(max_length=500, verbose_name='Описание')
+    image=models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='Изображение')
     category=models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     created_at=models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
@@ -20,5 +20,13 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='наименование', help_text='Введите наименование')
-    description = models.TextField(max_length=500, verbose_name='описание')
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование')
+    description = models.TextField(max_length=500, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural='Категории'
