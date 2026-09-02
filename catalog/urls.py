19461b4243057d370937a996e1b5af2bc894de89
ipavels <sirzaitsev@yandex.ru>
2026-09-02 09:29:39 +0300

    onecomit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
+from . import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
+    path("products/<int:pk>/", views.product_detail, name="product_detail"),
     path('contacts/',contacts, name='contacts'),
 ]
