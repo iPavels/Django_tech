a3b2e2c830bec0fceb823dd55f66e3d4fe7fd261
ipavels <sirzaitsev@yandex.ru>
2026-09-16 11:26:18 +0300

    cbv1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
+
 from catalog.apps import CatalogConfig
+
 from . import views
 
 
 app_name = CatalogConfig.name
 
-
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("products/<int:pk>/", views.product_detail, name="product_detail"),
-    path("contacts/", views.contacts, name="contacts"),
+    path("", views.ProductListView.as_view(), name="home"),
+    path(
+        "products/<int:pk>/",
+        views.ProductDetailView.as_view(),
+        name="product_detail",
+    ),
+    path("contacts/", views.ContactsView.as_view(), name="contacts"),
 ]
